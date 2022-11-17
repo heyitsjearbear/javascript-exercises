bc98e36671ab5db3e0ee2cb8f72f4d5460cc86d7
Jeremiah Ramiscal <jereberries1215@gmail.com>
2022-11-16 17:12:08 -0800

    Finished 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,24 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+    let repeatedString = "";
+    if(times>1){
+        for(let i = 0; i<times; i++){
+            repeatedString+=word;
+        }
+    }
+    else if(times == 0){
+        return "";
+    }
+    else if(times<0){
+        return "ERROR";
+    }
+    else if(word == ''){
+        return "";
+    }
+    else{
+        return word;
+    }
+
+    return repeatedString;
 
 };
 
