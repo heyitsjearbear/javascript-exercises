a1809ee4becfd24e6ddccab41e0cbe035770f7c1
Jeremiah Ramiscal <jereberries1215@gmail.com>
2022-11-18 19:35:56 -0800

    .

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(int1,int2) {
+    
+    if(!Number.isInteger(int1) || !Number.isInteger(int2)){
+        return "ERROR";
+    }
+    if(int1<0 || int2<0){
+        return "ERROR";
+    }
 
+    let max;
+    let min;
+    if(int1>int2){
+        max = int1;
+        min = int2;
+    }
+    else{
+        max = int2;
+        min = int1;
+    }
+    let sum = 0;
+    for(let i = min; i<max+1; i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
