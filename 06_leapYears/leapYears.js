58ff1cb27080f57ed13d4b891727a8222a266270
Jeremiah Ramiscal <jereberries1215@gmail.com>
2022-11-18 20:01:56 -0800

    .

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
+const leapYears = function(leap) {
+    if(leap % 4 == 0 ){
+        if(leap%100==0){
+            if(leap%400==0){
+                return true;
+            }
+            return false;
+        }
+        return true;
 
+    }
+    else if(leap%400==0){
+        return true;
+    }
+    return false;
 };
-
 // Do not edit below this line
 module.exports = leapYears;
